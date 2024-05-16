864b9152c8da4cc97c7eff514f748eca23a198e7
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-05-16 17:22:25 +0200

    feat(server): add .env and basic server init config

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log('ok');
+const http = require('http');
+require('dotenv').config();
+const { APP_PORT, APP_LOCALHOST } = process.env;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello world!');
+});
+
+server.listen(APP_PORT || 9000, APP_LOCALHOST, () => {
+  console.log(`Server is running at http://${APP_LOCALHOST}:${APP_PORT}`);
+});
