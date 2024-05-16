8475f29d037ec8bef3a3a548ef1931b814b852aa
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-05-16 17:28:20 +0200

    feat(server.js): started implementing pug with a basic template.pug file

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const http = require('http');
+const pug = require('pug');
 require('dotenv').config();
 const { APP_PORT, APP_LOCALHOST } = process.env;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
+  try {
+    const data = {
+      username: 'Mickaël',
+    };
+    const compile = pug.compileFile('./views/template.pug', { pretty: true });
+    const result = compile(data);
+
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(result);
+  } catch (err) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Erreur lors de la lecture du fichier');
+  }
 });
 
 server.listen(APP_PORT || 9000, APP_LOCALHOST, () => {
