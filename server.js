88f92b21973f804c01f6dfc0dde1b9498413fceb
Mickaël Mesloub <mickael.mesloub@gmail.com>
2024-05-17 14:51:35 +0200

    feat(server.js) add new capitalize fn to capitalize students name and replace "users" by "students"

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const { readFileSync } = fs;
 const { APP_ENV, APP_PORT, APP_LOCALHOST } = process.env;
 
 // functions from utils file
-const { defineEnvMessage, handleError, formatDate } = u;
+const { defineEnvMessage, handleError, formatDate, capitalize } = u;
 
 const server = createServer((req, res) => {
   try {
@@ -23,7 +23,6 @@ const server = createServer((req, res) => {
     if (url && url.includes('styles')) {
       try {
         const css = readFileSync(`${__dirname}/${url}`);
-
         res.writeHead(200, { 'Content-Type': 'text/css' });
         res.write(css);
         res.end();
@@ -31,7 +30,6 @@ const server = createServer((req, res) => {
         const message = 'Error loading the stylesheet file';
         handleError({ env: APP_ENV, res, statusCode: 404, message, err });
       }
-
       return;
     }
 
@@ -49,24 +47,8 @@ const server = createServer((req, res) => {
     const { students } = JSONFile;
 
     if (!students) {
-      const message = 'Error retrieving users';
+      const message = 'Error retrieving students';
       handleError({ env: APP_ENV, res, statusCode: 404, message });
-
-      return;
-    }
-
-    let result;
-    try {
-      // compile pug home file to send it in response to client
-      const compile = compileFile('./views/home.pug', { pretty: true });
-      result = compile({ students, formatDate });
-
-      res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-      res.end(result);
-    } catch (err) {
-      const message = 'Error compiling the Pug template';
-      handleError({ env: APP_ENV, res, statusCode: 500, message, err });
-
       return;
     }
 
@@ -76,44 +58,76 @@ const server = createServer((req, res) => {
         body += chunk;
       });
       req.on('end', () => {
-        let name;
-        let birth;
-        console.log(body);
-
         try {
+          let name;
+          let birth;
+          console.log(body);
+
           // regex to check if we receive name={name}&birth={date}
           const regex = /name=([^&]+)&birth=([^&]+)/;
 
           // check if the response body matches the regex
           const match = body.match(regex);
 
-          // asign values to name and birth if it matches
+          // assign values to name and birth if it matches
           if (match) {
-            name = match[1];
+            name = capitalize(match[1]);
             birth = match[2];
           }
 
           if (name && birth) {
             students.push({ name, birth });
-          }
 
-          const newData = JSON.stringify({ students });
-          fs.writeFileSync('./Data/data.json', newData);
-          console.log('JSON file updated successfully!');
-          console.log(students);
-          res.end();
-          return;
+            const newData = JSON.stringify({ students });
+            fs.writeFileSync('./Data/data.json', newData);
+            console.log('JSON file updated successfully!');
+            console.log(students);
+
+            // redirect to /students after handling the request
+            res.writeHead(301, { Location: '/students' });
+            res.end();
+          } else {
+            const message = 'Invalid data format';
+            handleError({ env: APP_ENV, res, statusCode: 400, message });
+          }
         } catch (err) {
-          const message = '';
-          handleError({ APP_ENV, res, statusCode: 404, message, err });
+          const message = 'Error processing the form data';
+          handleError({ env: APP_ENV, res, statusCode: 500, message, err });
         }
       });
+      return;
+    }
+
+    if (url === '/' && method === 'GET') {
+      try {
+        // compile pug home file to send it in response to client
+        const compile = compileFile('./views/home.pug', { pretty: true });
+        const result = compile({ students, formatDate });
+
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(result);
+      } catch (err) {
+        const message = 'Error compiling the Pug template';
+        handleError({ env: APP_ENV, res, statusCode: 500, message, err });
+      }
+    } else if (url === '/students' && method === 'GET') {
+      console.log(url, method);
+
+      try {
+        // compile pug home file to send it in response to client
+        const compile = compileFile('./views/students.pug', { pretty: true });
+        const result = compile({ students, formatDate });
+
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(result);
+      } catch (err) {
+        const message = 'Error compiling the Pug template';
+        handleError({ env: APP_ENV, res, statusCode: 500, message, err });
+      }
     }
   } catch (err) {
     const message = 'Internal server error';
     handleError({ env: APP_ENV, res, statusCode: 500, message, err });
-
-    return;
   }
 });
 
